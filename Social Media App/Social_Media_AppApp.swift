94f894d4c58cd86fe161bb52f58bb2b901022936
abdusamad <abdusamad2609@gmail.com>
2024-01-15 15:05:52 +0500

    Initial Commit

---
+++ Social Media App/Social_Media_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Media_AppApp.swift
+//  Social Media App
+//
+//  Created by Abdusamad Mamasoliyev on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Social_Media_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
